3123250f62f6d16db0d7420da508791497f0f957
lakshika <lakshikamedhavini@gmail.com>
2023-05-05 18:33:16 +0530

    login created

--- 22_23-j-17/App.js
+++ 22_23-j-17/App.js
@@ -1,105 +1,88 @@
-import {NavigationContainer} from "@react-navigation/native"
-import {createStackNavigator} from "@react-navigation/stack"
-import React,{useState,useRef,useEffect} from 'react'
-import { Button } from 'react-native';
-import {firebase} from './global/firebase'
-import Header from './components/Header'
-import Login from './components/Login'
-import Registration from './components/Registration'
-import Dashboard from './components/Dashboard'
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import React, { useState, useRef, useEffect } from "react";
+import { Button } from "react-native";
+import { firebase } from "./global/firebase";
+import Header from "./components/Header";
+import Login from "./components/Login";
+import Registration from "./components/Registration";
+import Dashboard from "./components/Dashboard";
 
-import { LogBox } from 'react-native';
+import { LogBox } from "react-native";
 LogBox.ignoreAllLogs();
-
 
 const Stack = createStackNavigator();
 
-function App(){
-  const [initializing,setInitializing] =useState(true);
-  const [user,setUser] = useState();
+function App() {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
 
   //handle use state changes
-  function onAuthStateChanged(user){
+  function onAuthStateChanged(user) {
     setUser(user);
     if (initializing) setInitializing(false);
   }
 
-  useEffect(() =>{
+  useEffect(() => {
     const subscriber = firebase.auth().onAuthStateChanged(onAuthStateChanged);
     return subscriber;
-  },
-  []);
+  }, []);
 
   if (initializing) return null;
 
-  if (!user){
+  if (!user) {
     return (
       <Stack.Navigator>
         <Stack.Screen
-        name = "Login"
-        component = {Login}
-        options ={{
-          headerTitle: () => <Header name = "Login"/>,
-          headerShown: false ,
-          headerStyle:{
-            height:100,
-            backgroundColor:"#00e4d0",
-            shadowColor:'#000',
-            elevation:25
-          }
-        }}
+          name="Login"
+          component={Login}
+          options={{
+            headerTitle: () => <Header name="Login" />,
+            headerShown: false,
+          }}
         />
 
-      <Stack.Screen
-        name = "Registration"
-        component = {Registration}
-        options ={{
-          headerTitle: () => <Header name = "Registration"/>,
-          headerStyle:{
-            height:150,
-            backgroundColor:"#00e4d0",
-            shadowColor:'#000',
-            elevation:25
-          }
-        }}
-        />    
-
+        <Stack.Screen
+          name="Registration"
+          component={Registration}
+          options={{
+            headerTitle: () => <Header name="Registration" />,
+            headerShown: false,
+          }}
+        />
       </Stack.Navigator>
     );
   }
 
-  return(
+  return (
     <Stack.Navigator>
       <Stack.Screen
-        name = "Dashboard"//"Dashboard"
-        component = {Dashboard}
-        options ={{
-          headerTitle: () => <Header name = "Dashboard"/>,
-          headerStyle:{
-            height:70,
+        name="Dashboard" //"Dashboard"
+        component={Dashboard}
+        options={{
+          headerTitle: () => <Header name="Dashboard" />,
+          headerStyle: {
+            height: 70,
             // borderBottomLeftradius:50,
             // borderBottomRightradius:50,
-            backgroundColor:"#00e4d0",
-            shadowColor:'#000',
-            elevation:25
+            backgroundColor: "#00e4d0",
+            shadowColor: "#000",
+            elevation: 25,
           },
           headerRight: () => (
-            <Button
-              title="Logout"
-              onPress={() => firebase.auth().signOut()}
-            />
+            <Button title="Logout" onPress={() => firebase.auth().signOut()} />
           ),
           headerShown: false,
         }}
-        /> 
+      />
     </Stack.Navigator>
   );
-      }
+}
 
-export default () =>{
-    return(
-      <NavigationContainer>
-        <App/>
-      </NavigationContainer>
-    )
-  }
+export default () => {
+  return (
+    <NavigationContainer>
+      <App />
+    </NavigationContainer>
+  );
+};
--- 22_23-j-17/components/Login.js
+++ 22_23-j-17/components/Login.js
@@ -1,12 +1,25 @@
-import { View, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
-import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+  Dimensions,
+  Image,
+} from "react-native";
+import React, { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { firebase } from '../global/firebase';
+import { firebase } from "../global/firebase";
+import { MaterialIcons } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Avatar, Button } from "react-native-paper";
+
+const { height, width } = Dimensions.get("window");
 
 const Login = () => {
   const navigation = useNavigation();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const loginUser = async (email, password) => {
     try {
@@ -18,48 +31,71 @@ const Login = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={{ fontWeight: 'bold', fontSize: 26 }}>Login</Text>
-      <View style={{ fontWeight: 'bold', fontSize: 26 }}>
-        <TextInput
-          style={styles.textInput}
-          placeholder='Email'
-          onChangeText={(email) => setEmail(email)}
-          autoCapitalize='none'
-          autoCorrect={false}
-        />
-        <TextInput
-          style={styles.textInput}
-          placeholder='Password'
-          onChangeText={(password) => setPassword(password)}
-          autoCapitalize='none'
-          autoCorrect={false}
-          secureTextEntry={true}
-        />
+      <Image
+        source={require("../assets/loginbac.jpg")}
+        style={styles.backgroundImage}
+      />
+      <View style={styles.loginform}>
+        <View style={styles.loingImg}>
+          <Avatar.Image
+            source={require("../assets/logo.png")}
+            style={{ backgroundColor: "white" }}
+            size={200}
+          />
+        </View>
+        <View style={styles.textInputContainer}>
+          <View style={styles.input}>
+            <MaterialIcons name="email" size={25} color="green" />
+            <TextInput
+              style={styles.inputInside}
+              onChangeText={setEmail}
+              value={email}
+              placeholder="Email"
+            />
+          </View>
+          <View style={styles.input}>
+            <MaterialCommunityIcons
+              name="form-textbox-password"
+              size={24}
+              color="green"
+            />
+            <TextInput
+              secureTextEntry={true}
+              style={styles.inputInside}
+              onChangeText={setPassword}
+              value={password}
+              placeholder="Password"
+            />
+          </View>
+        </View>
+        <TouchableOpacity
+          onPress={() => loginUser(email, password)}
+          style={styles.button}
+        >
+          <Text style={{ fontWeight: "bold", fontSize: 22, color: "white" }}>
+            Login
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Registration")}
+          style={{ marginTop: 20 }}
+        >
+          <Text style={{ fontWeight: "bold", fontSize: 16, color: "white" }}>
+            Register Now
+          </Text>
+        </TouchableOpacity>
       </View>
-      <TouchableOpacity
-        onPress={() => loginUser(email, password)}
-        style={styles.button}
-      >
-        <Text style={{ fontWeight: "bold", fontSize: 22 }}>Login</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => navigation.navigate('Registration')}
-        style={{ marginTop: 20 }}
-      >
-        <Text style={{ fontWeight: "bold", fontSize: 16 }}>
-          Register Now
-        </Text>
-      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: "center",
+    justifyContent: "center", // added to center content
+    paddingHorizontal: width / 17,
+    paddingTop: height / 13,
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center', // added to center content
-    marginTop: 10
   },
   textInput: {
     paddingTop: 20,
@@ -69,18 +105,78 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: "#000",
     marginBottom: 10,
-    textAlign: 'center'
+    textAlign: "center",
   },
   button: {
-    marginTop: 50,
-    height: 70,
-    width: 250,
+    marginTop: "6%",
+    height: "7%",
+    width: "80%",
     fontSize: 20,
-    backgroundColor: "#026efd",
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     borderRadius: 50,
-  }
+    borderColor: "white",
+    borderWidth: 3,
+  },
+  input: {
+    flexDirection: "row",
+    backgroundColor: "#F1F6FB",
+    padding: 13,
+    // paddingTop: 10,
+    // paddingBottom: 100,
+    margin: 15,
+    marginTop: 0,
+    borderRadius: 4,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 0,
+    opacity: 1,
+    borderColor: "3085E22",
+    borderWidth: 1,
+  },
+  inputInside: {
+    paddingLeft: 10,
+    width: width,
+    opacity: 1,
+  },
+  textInputContainer: {
+    width: "90%",
+    height: "27%",
+    marginTop: "10%",
+    justifyContent: "center",
+    backgroundColor: "white",
+    marginHorizontal: "2%",
+    borderRadius: 10,
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: "cover",
+  },
+  loginform: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    width: "100%",
+    height: "100%",
+  },
+  loingImg: {
+    width: "40%",
+    height: "30%",
+    alignContent: "center",
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 
 export default Login;
