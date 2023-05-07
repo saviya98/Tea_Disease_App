d17a4ab2edababbeb0d554b5cd40332c3aa2e35a
lakshika <lakshikamedhavini@gmail.com>
2023-05-07 17:06:43 +0530

    count and home done

--- components/BlisterSample.js
+++ components/BlisterSample.js
@@ -1,9 +1,18 @@
-import { View, Text, Button, StyleSheet ,Image } from 'react-native';
-import React, { useRef, useEffect, useState } from 'react';
-import * as ImagePicker from 'expo-image-picker';
-import axios from 'axios';
-import { firebase } from '../global/firebase';
-import BlisterSampleResult from './BlisterSampleResult';
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
+import React, { useRef, useEffect, useState } from "react";
+import * as ImagePicker from "expo-image-picker";
+import axios from "axios";
+import { firebase } from "../global/firebase";
+import BlisterSampleResult from "./BlisterSampleResult";
+const { height, width } = Dimensions.get("window");
 
 const BlisterSample = () => {
   const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
@@ -15,14 +24,15 @@ const BlisterSample = () => {
 
   const [blisterSampleData, setBlisterSampleData] = useState(null);
   const [translucent, setTranslucent] = useState(null);
-  const [blisterUnder, setBlisterUnder] = useState(null); 
-  const [blisterUpper, setBlisterUpper] = useState(null); 
-  const [necro, setNecro] = useState(null); 
+  const [blisterUnder, setBlisterUnder] = useState(null);
+  const [blisterUpper, setBlisterUpper] = useState(null);
+  const [necro, setNecro] = useState(null);
 
   useEffect(() => {
     (async () => {
-      const galleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
-      setHasGalleryPermission(galleryStatus.status === 'granted');
+      const galleryStatus =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+      setHasGalleryPermission(galleryStatus.status === "granted");
     })();
   }, []);
 
@@ -31,160 +41,172 @@ const BlisterSample = () => {
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       quality: 1,
     });
-  
+
     console.log(result);
-  
+
     if (!result.canceled) {
       const formData = new FormData();
-      formData.append('image', {
+      formData.append("image", {
         uri: result.assets[0].uri,
-        name: 'test.jpg',
-        type: 'image/jpeg'
+        name: "test.jpg",
+        type: "image/jpeg",
       });
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////      
-      axios.post('http://192.168.8.100:3009/geo', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      }).then(async (response) => {
+      ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      axios
+        .post("http://192.168.8.100:3009/geo", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        })
+        .then(async (response) => {
+          setImage(result.assets[0].uri);
+          // console.log("Response ",response);
+          setGeoData(response.data);
+          setLatitude(response.data.lat[0]);
+          setLongitude(response.data.lon[0]);
+          setDate(response.data.date[0]);
 
-        setImage(result.assets[0].uri);
-        // console.log("Response ",response);
-        setGeoData(response.data);
-        setLatitude(response.data.lat[0]);
-        setLongitude(response.data.lon[0]);
-        setDate(response.data.date[0]);
-  
-        console.log(response.data.lat[0]); 
-        console.log(response.data.lon[0]); 
-        console.log(response.data.date[0]);
-        
-      }).catch((error) => {
-        console.log(error);
-      });
+          console.log(response.data.lat[0]);
+          console.log(response.data.lon[0]);
+          console.log(response.data.date[0]);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
 
-      axios.post('http://192.168.8.100:3009/blisterSample', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      }).then(async (response2) => {
+      axios
+        .post("http://192.168.8.100:3009/blisterSample", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        })
+        .then(async (response2) => {
+          setImage(result.assets[0].uri);
+          // console.log("Response ",response);
+          setBlisterSampleData(response2.data);
+          setTranslucent(response2.data.Translucent[0]);
+          setBlisterUnder(response2.data.BlisterUnder[0]);
+          setNecro(response2.data.Necro[0]);
+          setBlisterUpper(response2.data.BlisterUpper[0]);
 
-        setImage(result.assets[0].uri);
-        // console.log("Response ",response);
-        setBlisterSampleData(response2.data);
-        setTranslucent(response2.data.Translucent[0]);
-        setBlisterUnder(response2.data.BlisterUnder[0]);
-        setNecro(response2.data.Necro[0]);
-        setBlisterUpper(response2.data.BlisterUpper[0]);
-  
-        console.log('Translucent',response2.data.Translucent[0]); 
-        console.log(response2.data.BlisterUnder[0]); 
-        console.log(response2.data.Necro[0]);
-      }).catch((error) => {
-        console.log(error);
-      });
+          console.log("Translucent", response2.data.Translucent[0]);
+          console.log(response2.data.BlisterUnder[0]);
+          console.log(response2.data.Necro[0]);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
 
-       // Create a reference to the Firebase Storage bucket
-       const storageRef = firebase.storage().ref().child('SeveritySample/' + image.split('/').pop());
-  
-       // Upload the image to Firebase Storage
-       const responseUrl = await fetch(image);
-       const blob = await responseUrl.blob();
-       const snapshot = await storageRef.put(blob);
- 
-       // Create a reference to the Firestore collection
-       const collectionRef1 = firebase.firestore().collection('SeveritySample');
-       console.log("HIII1");
-       // Add the image metadata to the Firestore collection
-       collectionRef1.add({
-         image: await snapshot.ref.getDownloadURL(),
-         date: date,
-         geo: {
-           latitude: parseFloat(latitude),
-           longitude: parseFloat(longitude)
-         },
-         sampleData: {
-          Translucent: translucent+'%',
-          Blister_Upper: blisterUpper+'%',
-          Blister_Under: blisterUnder+'%',
-          Necro: necro+'%',
-        }
-       }).then((docRef) => {
-         console.log("Document written with ID: ", docRef.id);
-       }).catch((error) => {
-         console.error("Error adding document: ", error);
-       });
-       console.log("HIII2");
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////   
+      // Create a reference to the Firebase Storage bucket
+      const storageRef = firebase
+        .storage()
+        .ref()
+        .child("SeveritySample/" + image.split("/").pop());
 
+      // Upload the image to Firebase Storage
+      const responseUrl = await fetch(image);
+      const blob = await responseUrl.blob();
+      const snapshot = await storageRef.put(blob);
 
+      // Create a reference to the Firestore collection
+      const collectionRef1 = firebase.firestore().collection("SeveritySample");
+      console.log("HIII1");
+      // Add the image metadata to the Firestore collection
+      collectionRef1
+        .add({
+          image: await snapshot.ref.getDownloadURL(),
+          date: date,
+          geo: {
+            latitude: parseFloat(latitude),
+            longitude: parseFloat(longitude),
+          },
+          sampleData: {
+            Translucent: translucent + "%",
+            Blister_Upper: blisterUpper + "%",
+            Blister_Under: blisterUnder + "%",
+            Necro: necro + "%",
+          },
+        })
+        .then((docRef) => {
+          console.log("Document written with ID: ", docRef.id);
+        })
+        .catch((error) => {
+          console.error("Error adding document: ", error);
+        });
+      console.log("HIII2");
+      ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     }
   };
 
   if (image && translucent && blisterUpper && blisterUnder && necro) {
-    return <BlisterSampleResult image={image} translucent={translucent} necro={necro} blisterUpper={blisterUpper} blisterUnder={blisterUnder}/>;
+    return (
+      <BlisterSampleResult
+        image={image}
+        translucent={translucent}
+        necro={necro}
+        blisterUpper={blisterUpper}
+        blisterUnder={blisterUnder}
+      />
+    );
   } else if (hasGalleryPermission === false) {
     return <Text>No access to Internal Storage</Text>;
   } else {
     return (
-      <View style={{ flex: 1 }}>
-  <Text style={{ fontSize: 20, textAlign: 'center', marginTop: 30 ,marginLeft:10, fontWeight: 'bold'}}>
+      <View style={styles.container}>
+        {/* <Text style={{ fontSize: 20, textAlign: 'center', marginTop: 30 ,marginLeft:10, fontWeight: 'bold'}}>
     Please use the quadrant shape object like the below image. 
-  </Text>    
-  <View style={{ alignItems: 'center', marginTop: 15, marginBottom: 30 }}>
-    <Image
-      source={require('../assets/IMG_3875.jpg')}
-      style={[styles.image, { resizeMode: 'contain', width: 500, height: 400 }]}
-    />
-  </View>      
-  <View style={{  justifyContent: 'space-between', paddingHorizontal: 60 }}>
-    <Button title='Camera' onPress={() => pickImage()} style={{ width: '40%', marginBottom: 20 }} />
-    <View style={{ height: 20 }} />
-    <Button title='Gallery' onPress={() => pickImage()} style={{ width: '40%' }} />
-  </View>
-  {/* {image && <Image source={{ uri: image }} style={{ flex: 1 / 2 }} />} */}
-</View>
+  </Text>     */}
 
-
-
+        <View style={styles.imageContainer}>
+          <Image
+            source={require("../assets/IMG_3875.jpg")}
+            style={styles.image}
+          />
+        </View>
+        <View style={styles.btnContainer}>
+          <TouchableOpacity onPress={() => pickImage()} style={styles.btn}>
+            <Text style={styles.btnTxt}>Take a Photo</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => pickImage()} style={styles.btn}>
+            <Text style={styles.btnTxt}>Upload a Photo</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     );
   }
-  
-  
 };
 
 export default BlisterSample;
 
-
-
 const styles = StyleSheet.create({
-  container:{
-      flex:1,
-      alignItems:'center',
-      marginTop:10
+  container: {
+    flex: 1,
+    paddingHorizontal: width / 23,
+    paddingTop: height / 60,
+    marginBottom: height / 10,
   },
-  textInput:{
-    paddingTop:20,
-    paddingBottom:10,
-    width:400,
-    fonySize:20,
-    borderBottomWidth:1,
-    borderBottomColor:"#000",
-    marginBottom:10,
-    textAlign:'center'
+  imageContainer: {
+    width: "100%",
+    height: "80%",
+    borderRadius: 9,
   },
-  
-  button:{
-      marginTop:50,
-      height:70,
-      width:250,
-      fonySize:20,
-      backgroundColor:"#026efd",
-      alignItems:'center',
-      justifyContent:'center',
-      borderRadius:50,
-    },
-
-
-
-})
+  btnContainer: {
+    width: "100%",
+    height: "20%",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignSelf: "center",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 9,
+  },
+  btn: {
+    alignSelf: "center",
+    borderColor: "#085E22",
+    borderWidth: 1,
+    borderRadius: 9,
+  },
+  btnTxt: { padding: "3%", fontWeight: "bold", color: "#085E22", fontSize: 19 },
+});
--- components/Blister_Map_All.js
+++ components/Blister_Map_All.js
@@ -1,70 +1,68 @@
 //importing components and modules
-import { StyleSheet, Text, View,Dimensions,Image} from 'react-native'
-import React,{useState,useRef,useEffect} from 'react'
-const SCREEN_WIDTH =Dimensions.get('window').width
-import {colors,parameters} from '../global/styles'
-import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, Dimensions, Image } from "react-native";
+import React, { useState, useRef, useEffect } from "react";
+const SCREEN_WIDTH = Dimensions.get("window").width;
+import { colors, parameters } from "../global/styles";
+import { StatusBar } from "expo-status-bar";
 //access blister identified locations data in the data file
 //import { blisterAround } from '../global/data'
-import MapView, { PROVIDER_GOOGLE,Marker } from 'react-native-maps'; 
-import * as Location from 'expo-location';
-import * as geolib from 'geolib';
-import {firebase} from '../global/firebase'
-
-
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
+import * as Location from "expo-location";
+import * as geolib from "geolib";
+import { firebase } from "../global/firebase";
 
 const Blister_Map_All = () => {
-
-//get user permission to access device location data
-const checkPermission =async()=>{
+  //get user permission to access device location data
+  const checkPermission = async () => {
     const hasPermission = await Location.requestForegroundPermissionsAsync();
-    if(hasPermission.status === 'granted') {
-        const permission = await askPermission();
-        return permission
+    if (hasPermission.status === "granted") {
+      const permission = await askPermission();
+      return permission;
     }
-    return true
-};
+    return true;
+  };
 
+  const askPermission = async () => {
+    const permission = await Location.requestForegroundPermissionsAsync();
+    return permission.status === "granted";
+  };
 
-const askPermission = async()=>{
-    const permission = await Location.requestForegroundPermissionsAsync()
-    return permission.status === 'granted';
-};
-
-//get device's location data
-const getLocation = async() => {
-  try {
+  //get device's location data
+  const getLocation = async () => {
+    try {
       const { granted } = await Location.requestForegroundPermissionsAsync();
       if (!granted) return;
       const {
-          coords: { latitude, longitude },
+        coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
       setLocation({ latitude, longitude });
-  } catch (err) {
+    } catch (err) {
       // Handle the error here
-  }
-}
+    }
+  };
 
-const [location, setLocation] = useState({});
+  const [location, setLocation] = useState({});
 
   const _map = useRef(1);
 
-  useEffect(()=>{
+  useEffect(() => {
     checkPermission();
-    getLocation()
-,[]})
+    getLocation(), [];
+  });
 
-//fucntion to get current location
-const useCurrentLocation = async () => {
+  //fucntion to get current location
+  const useCurrentLocation = async () => {
     let current_location = await Location.getCurrentPositionAsync({});
     return current_location.coords;
   };
 
- 
- const [current_location, setLocation2] = React.useState({latitude: 0, longitude: 0});
+  const [current_location, setLocation2] = React.useState({
+    latitude: 0,
+    longitude: 0,
+  });
 
   React.useEffect(() => {
-    useCurrentLocation().then(coords => {
+    useCurrentLocation().then((coords) => {
       setLocation2(coords);
     });
   }, []);
@@ -72,37 +70,37 @@ const useCurrentLocation = async () => {
   // console.log(current_location.longitude);
 
   /// Loch test ///
-  fetch('http://192.168.8.100:3009/location', {
-    method: 'POST',
+  fetch("http://192.168.8.100:3009/location", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       // your data here
-      "current_latitude": current_location.latitude,
-      "current_longitude": current_location.longitude
-    })
-  })
+      current_latitude: current_location.latitude,
+      current_longitude: current_location.longitude,
+    }),
+  });
 
   // initialized variables
   const [blister_locations, setUsers_blocations] = useState([]);
   //make reference for the database collection
-  const todoRef = firebase.firestore().collection('blisterC');
+  const todoRef = firebase.firestore().collection("blisterC");
 
-  //useEffect hook is used to fetch data from the Firestore collection and update the state variable, 
+  //useEffect hook is used to fetch data from the Firestore collection and update the state variable,
   //// fetch data from the Firebase database collection
   useEffect(() => {
     const unsubscribe = todoRef.onSnapshot((querySnapshot) => {
       const blister_locations = [];
       querySnapshot.forEach((doc) => {
-        const { color, geo} = doc.data();
+        const { color, geo } = doc.data();
         blister_locations.push({
           //fields need to be extracted from the firestore collection
           color,
           geo,
         });
       });
-      setUsers_blocations(blister_locations);//update the blister_locations state variable
+      setUsers_blocations(blister_locations); //update the blister_locations state variable
     });
 
     return () => {
@@ -115,151 +113,154 @@ const useCurrentLocation = async () => {
   const filteredLocations = blister_locations.filter((item) => {
     //geolib library to calculate the distance between the user's location and the nearby locations
     const distance = geolib.getDistance(current_location, item.geo);
-    return distance ;
+    return distance;
   });
 
   //store the filtered locations
-  const near_Locations = filteredLocations
+  const near_Locations = filteredLocations;
 
   return (
-    <View style ={styles.container}>
-      
-                    <View style ={{alignItems:"center",justifyContent:"center"}}>
-                    {/*Dispaly the Map*/}                        
-                    <MapView
-                           // ref = {_map}
-                            provider ={PROVIDER_GOOGLE}
-                            style = {styles.map}
-                            region = {{
-                              //focusing latitude and longtitide of Sri lanka
-                              latitude: 7.8731,
-                              longitude: 80.7718,
-                              latitudeDelta: 2.5,
-                              longitudeDelta: 2.5
-                            }}
-                         >
-                      {/* visualize blister identified locaions according to the identified stages with relevant pin colors */}
-                        {near_Locations.map((item,index)=>
-                            <Marker 
-                            coordinate = {item.geo} 
-                            key= {index.toString()} 
-                            pinColor={item.color}/>                               
-                            )
-                            }
+    <View style={styles.container}>
+      <View style={{ alignItems: "center", justifyContent: "center" }}>
+        {/*Dispaly the Map*/}
+        <MapView
+          // ref = {_map}
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          region={{
+            //focusing latitude and longtitide of Sri lanka
+            latitude: 7.8731,
+            longitude: 80.7718,
+            latitudeDelta: 2.5,
+            longitudeDelta: 2.5,
+          }}
+        >
+          {/* visualize blister identified locaions according to the identified stages with relevant pin colors */}
+          {near_Locations.map((item, index) => (
+            <Marker
+              coordinate={item.geo}
+              key={index.toString()}
+              pinColor={item.color}
+            />
+          ))}
+        </MapView>
+      </View>
+      <StatusBar style="light" backgroundColor="#2058c0" translucent={true} />
 
-                       </MapView>   
-                    </View>
-      <StatusBar style ="light" backgroundColor="#2058c0" translucent={true}/>
-
-{/* add a risk level card for better user experience */}
+      {/* add a risk level card for better user experience */}
       <View style={styles.colorIndicatorContainer}>
         <View style={styles.colorIndicatorCol}>
           <View style={styles.colorIndicator}>
             <Text style={styles.colorIndicatorText}>Risk level:</Text>
-            </View>
-            <View style={styles.colorIndicator}>
-              <Image source={require('../assets/icons8-delivery-pin-for-parcel-delivery-location-making-24_red.png')} style={styles.colorIndicatorIcon} />
-              <Text style={styles.colorIndicatorText}>High</Text>
-            </View>
-            <View style={styles.colorIndicator}>
-              <Image source={require('../assets/icons8-delivery-pin-for-parcel-delivery-location-making-24_orange.png')} style={styles.colorIndicatorIcon} />
-              <Text style={styles.colorIndicatorText}>Moderate</Text>
-            </View>
-            <View style={styles.colorIndicator}>
-              <Image source={require('../assets/icons8-delivery-pin-for-parcel-delivery-location-making-24_green.png')} style={styles.colorIndicatorIcon} />
-               <Text style={styles.colorIndicatorText}>Low</Text>
-             </View>
+          </View>
+          <View style={styles.colorIndicator}>
+            <Image
+              source={require("../assets/icons8-delivery-pin-for-parcel-delivery-location-making-24_red.png")}
+              style={styles.colorIndicatorIcon}
+            />
+            <Text style={styles.colorIndicatorText}>High</Text>
+          </View>
+          <View style={styles.colorIndicator}>
+            <Image
+              source={require("../assets/icons8-delivery-pin-for-parcel-delivery-location-making-24_orange.png")}
+              style={styles.colorIndicatorIcon}
+            />
+            <Text style={styles.colorIndicatorText}>Moderate</Text>
+          </View>
+          <View style={styles.colorIndicator}>
+            <Image
+              source={require("../assets/icons8-delivery-pin-for-parcel-delivery-location-making-24_green.png")}
+              style={styles.colorIndicatorIcon}
+            />
+            <Text style={styles.colorIndicatorText}>Low</Text>
+          </View>
         </View>
-      </View>         
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Blister_Map_All
+export default Blister_Map_All;
 
-//styles for the relavent items 
+//styles for the relavent items
 const styles = StyleSheet.create({
-        container:{
-            flex:1,
-            backgroundColor:colors.white,
-            paddingBottom:0,
-            paddingTop:0,
-            // paddingTop:parameters.statusBarHeight
-        },
-        header:{
-          backgroundColor:colors.blue,
-          height:parameters.headerHeight,
-          alignItems:"flex-start"
-         
-        },
-        
-        map:{          
-        height: 720,
-         marginVertical: 0,
-         width:SCREEN_WIDTH,
-         paddingBottom:0,
-            paddingTop:0,
-        },
-        text6:{
-          fontSize:15,
-          color:colors.black,
-          marginLeft:105,
-          marginRight:5,
-          marginTop:-8
-        },
-        text4:{
-          fontSize:30,
-          fontWeight:'bold',
-          color:colors.white,
-          marginLeft:200,
-          marginTop:-40
-        },
-        
-        icon1: {
-          marginLeft:10,
-          marginTop:5
-        },
-    
-       
-        blisterAround: {
-        width: 28,
-        height: 14,
-        
-        },    
-        
-      colorIndicatorContainer: {
-          position: 'absolute',
-          top: 0,
-          right: 0,
-          flexDirection: 'column',
-          alignItems: 'center',
-          padding: 20,
-          backgroundColor: 'rgba(255, 255, 255, 0.7)',
-          paddingBottom:10
-      },
-      colorIndicator: {
-          flexDirection: 'row',
-          alignItems: 'center',
-          padding: 2,
-          borderRadius: 10,
-          marginRight: 10,
-          alignSelf: 'flex-start',
-          justifyContent: 'flex-start'
-      },
-      colorIndicatorIcon: {
-          width: 15,
-          height: 15,
-          marginRight: 2,
-          marginLeft:-2
-      },
-      colorIndicatorText: {
-          fontWeight: 'bold',
-          color: 'black',
-          marginRight:-25
-      },
-      colorIndicatorCol: {
-        flexDirection: 'column',
-        alignItems: 'center'
-    }
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+    paddingBottom: 0,
+    paddingTop: 0,
+    // paddingTop:parameters.statusBarHeight
+  },
+  header: {
+    backgroundColor: "#085E22",
+    height: parameters.headerHeight,
+    alignItems: "flex-start",
+  },
 
-})
+  map: {
+    height: 720,
+    marginVertical: 0,
+    width: SCREEN_WIDTH,
+    paddingBottom: 0,
+    paddingTop: 0,
+  },
+  text6: {
+    fontSize: 15,
+    color: colors.black,
+    marginLeft: 105,
+    marginRight: 5,
+    marginTop: -8,
+  },
+  text4: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: colors.white,
+    marginLeft: 200,
+    marginTop: -40,
+  },
+
+  icon1: {
+    marginLeft: 10,
+    marginTop: 5,
+  },
+
+  blisterAround: {
+    width: 28,
+    height: 14,
+  },
+
+  colorIndicatorContainer: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    flexDirection: "column",
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: "rgba(255, 255, 255, 0.7)",
+    paddingBottom: 10,
+  },
+  colorIndicator: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 2,
+    borderRadius: 10,
+    marginRight: 10,
+    alignSelf: "flex-start",
+    justifyContent: "flex-start",
+  },
+  colorIndicatorIcon: {
+    width: 15,
+    height: 15,
+    marginRight: 2,
+    marginLeft: -2,
+  },
+  colorIndicatorText: {
+    fontWeight: "bold",
+    color: "black",
+    marginRight: -25,
+  },
+  colorIndicatorCol: {
+    flexDirection: "column",
+    alignItems: "center",
+  },
+});
